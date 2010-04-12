5c9bd2249b42ecf28dfdb0989c0770b45e448171
achiu <mr.arthur.chiu@gmail.com>
2010-04-12 01:29:23 -0700

    added hoptoad and organized templates

---
+++ plugins/hoptoad_plugin.rb
@@ -0,0 +1,14 @@
+# Template to get Hoptoad on Padrino
+# prereqs:
+# sudo gem install rack_hoptoad
+# http://github.com/atmos/rack_hoptoad
+HOPTOAD = <<-HOPTOAD
+    require 'rack/hoptoad'
+    app.use Rack::Hoptoad, 'API_KEY_HERE' do |notifier|
+      #notifier.report_under        << 'custom'
+      #notifier.environment_filters << %w(MY_SECRET_KEY MY_SECRET_TOKEN)
+    end
+HOPTOAD
+require_dependencies 'rack_hoptoad'
+initializer :hoptoad,HOPTOAD
+inject_into_file destination_root('/app/app.rb'),"    enable :raise_errors\n", :after => "configure do\n"
