2afc463d619647623db2da00ed3df5f06f69cfe4
achiu <mr.arthur.chiu@gmail.com>
2010-04-13 22:25:20 -0700

    added rack-maintenence plugin

---
+++ plugins/maintenance_plugin.rb
@@ -0,0 +1,19 @@
+# Maintanence plugin on Padrino
+# prereqs:
+# sudo gem install rack-maintenance
+# http://github.com/ddollar/rack-maintenance
+MAINT = <<-MAINT
+    require 'rack/maintenance'
+    app.use Rack::Maintenance,
+       :file => Padrino.root('public/maintenance.html')
+       # :env  => 'MAINTENANCE'
+MAINT
+PAGE = <<-PAGE
+<html>
+<head><title>Site Under Maintenance</title></head>
+<body><h1> This Site is Currently Under Maintenance!</h1>
+</html>
+PAGE
+require_dependencies 'rack-maintenance'
+initializer :maintenance,MAINT
+create_file 'public/maintenance.html',PAGE
