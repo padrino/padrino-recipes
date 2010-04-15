dd93eaa70296dd0cd7e6e33c73640a7cd8b104d8
achiu <mr.arthur.chiu@gmail.com>
2010-04-15 16:05:55 -0700

    [blueprint] minor fix

--- plugins/blueprint_plugin.rb
+++ plugins/blueprint_plugin.rb
@@ -7,7 +7,7 @@ ERB=<<-ERB
 ERB
 type = fetch_component_choice(:renderer)
 path = "http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/"
-create_file destination_root("app/views/shared/_blueprint.#{type}"), ( type == 'erb' ? ERB : ERB.gsub(/<|%|>/,''))
+create_file destination_root("app/views/shared/_blueprint.#{type}"), ( type == 'erb' ? ERB : ERB.gsub(/(<%)|(%>)/,''))
 get (path + "screen.css"), destination_root('public/stylesheets/screen.css')
 get (path + "print.css"), destination_root('public/stylesheets/print.css')
 say "Now, include the generated partial app/views/shared/_blueprint.#{type} into your layout!",:green
