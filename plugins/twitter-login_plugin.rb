621bcaea87a66ae17e0de90f49ae11d03aa49453
achiu <mr.arthur.chiu@gmail.com>
2010-04-15 09:03:42 -0700

    added twitter-login plugin

---
+++ plugins/twitter-login_plugin.rb
@@ -0,0 +1,16 @@
+# Twitter Login plugin on Padrino
+# prereqs:
+# sudo gem install twitter-login
+# http://github.com/mislav/twitter-login
+TWITTER = <<-TWITTER
+    require 'twitter/login'
+    app.use Twitter::Login, 
+      :consumer_key => 'KEY', 
+      :secret => 'SECRET'
+      # :login_path => '/login',
+      # :return_to => '/'
+    app.helpers Twitter::Login::Helpers
+TWITTER
+require_dependencies 'twitter-login', :require_as => 'twitter/login'
+initializer :twitter_login,TWITTER
+inject_into_file destination_root('app/app.rb'),"    enable :sessions\n", :after => "configure do\n"
