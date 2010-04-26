d35c8010a238bc1eb754ae426345530abe3bca3d
achiu <mr.arthur.chiu@gmail.com>
2010-04-26 16:24:31 -0700

    added google analytics

---
+++ plugins/googleanalytics_plugin.rb
@@ -0,0 +1,11 @@
+# Template to get Recaptcha on Padrino
+# prereqs:
+# sudo gem install rack-google-analytics
+# http://github.com/leehambley/rack-google-analytics
+# NOTE:
+ANALYTICS = <<-ANALYTICS
+    app.use Rack::GoogleAnalytics, 
+      :tracker => 'UA-xxxxxx-x'
+ANALYTICS
+require_dependencies 'rack-google-analytics'
+initializer :analytics,ANALYTICS
