518971b0b1e67ae6e13b03b36143c2e4def0d562
achiu <mr.arthur.chiu@gmail.com>
2010-05-19 17:48:32 -0700

    [recaptcha] update to version 0.2.0

--- plugins/recaptcha_plugin.rb
+++ plugins/recaptcha_plugin.rb
@@ -8,7 +8,7 @@ RECAPTCHA = <<-RECAPTCHA
     app.use Rack::Recaptcha,
       :private_key => "YOUR_PRIVATE_KEY",
       :public_key => "YOUR_PUBLIC_KEY", 
-      :login_path => "YOUR_LOGIN_PATH"
+      :paths => "YOUR_LOGIN_PATH(S)"
     app.helpers Rack::Recaptcha::Helpers
 RECAPTCHA
 require_dependencies 'rack-recaptcha', :require => 'rack/recaptcha'
