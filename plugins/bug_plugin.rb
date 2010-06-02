0357b0af1926b9c482cc80cf16570491a870dda4
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-02 12:32:13 -0700

    added optional configurations for rack-bug

--- plugins/bug_plugin.rb
+++ plugins/bug_plugin.rb
@@ -5,6 +5,18 @@
 BUG = <<-BUG
     app.use Rack::Bug,
       :secret_key => "someverylongandveryhardtoguesspreferablyrandomstring"
+    # extra configurations
+    # :password => 'password',
+    # :ip_masks   => [IPAddr.new("2.2.2.2/0")],
+    # :panel_classes => [
+    #     Rack::Bug::TimerPanel,
+    #     Rack::Bug::RequestVariablesPanel,
+    #     Rack::Bug::RedisPanel,
+    #     Rack::Bug::TemplatesPanel,
+    #     Rack::Bug::LogPanel,
+    #     Rack::Bug::MemoryPanel
+    #   ]
+    
 BUG
 require_dependencies 'rack-bug', :require => 'rack/bug'
 initializer :bug,BUG
