53caf620d7d6c2d3144c16f162492d21f9c1fea5
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-08-03 12:33:28 +0200

    Fixes to the 960 plugin.

--- plugins/960_plugin.rb
+++ plugins/960_plugin.rb
@@ -1,8 +1,8 @@
-%w(reset text 960).each do file
+%w(reset text 960).each do |file|
   get "http://github.com/nathansmith/960-Grid-System/raw/master/code/css/#{file}.css", "public/stylesheets/#{file}.css"
 end
 
-%w(12 16 24).each do file
+%w(12 16 24).each do |file|
   get "http://github.com/nathansmith/960-Grid-System/tree/master/code/img/#{file}_col.gif", "public/images/#{file}_col.gif"
 end
 
