2760c33501293876903e6bcf17be0798892b2db2
J. Edward Dewyea <jed@commuterdesign.com>
2010-09-20 09:41:36 -0400

    install spec.watchr when framework is Cucumber and nothing if no test framework

--- plugins/watchr_plugin.rb
+++ plugins/watchr_plugin.rb
@@ -1,5 +1,9 @@
 # Simple Auto-Test Watchr script generation.
 # TODO: add other types of watchr scripts.
 component = fetch_component_choice(:test)
-test = (component == 'rspec' ? 'spec' : 'test')
-get "http://github.com/padrino/padrino-recipes/raw/master/files/watchrs/#{test}.watchr", destination_root("#{test}.watchr")
+if component == 'none'
+  say "Installation cancelled. No testing framework found.", :red
+else
+  test = case component; when 'rspec', 'cucumber' then 'spec'; else 'test'; end
+  get "http://github.com/padrino/padrino-recipes/raw/master/files/watchrs/#{test}.watchr", destination_root("#{test}.watchr")
+end
