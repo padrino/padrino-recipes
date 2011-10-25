0e19570d6cf3dbe663562575704962da8b7416a4
Carlo Bertini <waydotnet@gmail.com>
2011-10-25 16:17:57 +0200

    refix issues #14

--- plugins/carrierwave_plugin.rb
+++ plugins/carrierwave_plugin.rb
@@ -84,10 +84,10 @@ end
 UPLOADER
 create_file destination_root('lib/uploader.rb'), UPLOADER
 generate :model, "upload file:text created_at:datetime"
-prepend_file destination_root('app/models/upload.rb'), "require 'carrierwave/orm/#{fetch_component_choice(:orm)}'\n"
+prepend_file destination_root('models/upload.rb'), "require 'carrierwave/orm/#{fetch_component_choice(:orm)}'\n"
 case fetch_component_choice(:orm)
 when 'datamapper'
-  inject_into_file destination_root('app/models/upload.rb'),", :auto_validation => false\n", :after => "property :file, Text"
+  inject_into_file destination_root('models/upload.rb'),", :auto_validation => false\n", :after => "property :file, Text"
 end
-inject_into_file destination_root('app/models/upload.rb'),"   mount_uploader :file, Uploader\n", :before => 'end'
+inject_into_file destination_root('models/upload.rb'),"   mount_uploader :file, Uploader\n", :before => 'end'
 require_dependencies 'carrierwave'
