8510e3ac640c768716f0bd2495d652e57a86a262
Carlo Bertini <waydotnet@gmail.com>
2012-01-27 09:49:10 +0100

    added instructions for using ActiveRecord 3.2 into DH

--- plugins/dreamhost_plugin.rb
+++ plugins/dreamhost_plugin.rb
@@ -7,7 +7,29 @@
 # goto to DH panel control and uncheck
 # Passenger (Ruby/Python apps only)
 #
+# TIPS :
 #
+# If you want to use ActiveRecord >= 3.2 (Padrino >= 0.10.6), you have to install a
+# new version of rubygems on Dreamhost, since the one installed is too
+# old (1.3.6) and therefore not compatible:
+# read the wiki dhreamhost  or follow the following steps:
+# $ cd ~ /
+# $ mkdir. Gem bin lib src
+# $ cd src
+# $ wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.15.tgz  (latest version at this time)
+# $ tar xvf rubygems-1.8.15.tgz
+# $ cd rubygems-1.8.15
+# $ ruby setup.rb - prefix = $ HOME
+# $ cd ~ / bin
+# $ ln-s gem1.8 gem
+# Add the following lines. Bash_profile
+# $ export PATH = "$ HOME / bin: $ HOME / .gems / bin: $ PATH"
+# $ export RUBYLIB = "$ HOME / lib: $ RUBYLIB"
+# then
+# $ source ~ /. Bash_profile
+# $ which gem # Should return / home / USERNAME / bin / gem
+# $ gem-v # Should return 1.8.15
+# DH wiki: http://wiki.dreamhost.com/RubyGems
 
 DISPATCH_FCGI = <<-DFCGI
 #!/usr/bin/ruby
