dae3f10c759327c9bcbab22f65149b0e0de872fa
Tema Bolshakov <abolshakov@spbtv.com>
2013-11-07 21:28:50 +0400

    Update twitter bootstrap plugin

--- plugins/bootstrap_plugin.rb
+++ plugins/bootstrap_plugin.rb
@@ -1,8 +1,12 @@
 # Simple plugin to install Twitter bootstrap.
 #
-get 'https://raw.github.com/twitter/bootstrap/master/docs/assets/css/bootstrap.css', destination_root('public/stylesheets/bootstrap.css')
-get 'https://raw.github.com/twitter/bootstrap/master/docs/assets/css/bootstrap-responsive.css', destination_root('public/stylesheets/bootstrap-responsive.css')
-get 'https://raw.github.com/twitter/bootstrap/master/docs/assets/js/bootstrap.js', destination_root('public/javascripts/bootstrap.js')
-get 'https://raw.github.com/twitter/bootstrap/master/docs/assets/js/bootstrap.min.js', destination_root('public/javascripts/bootstrap.min.js')
-get 'https://raw.github.com/twitter/bootstrap/master/docs/assets/img/glyphicons-halflings.png', destination_root('public/images/glyphicons-halflings.png')
-get 'https://raw.github.com/twitter/bootstrap/master/docs/assets/img/glyphicons-halflings-white.png', destination_root('public/images/glyphicons-halflings-white.png')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap.css', destination_root('public/stylesheets/bootstrap.css')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap-theme.css', destination_root('public/stylesheets/bootstrap-theme.css')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/js/bootstrap.js', destination_root('public/javascripts/bootstrap.js')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/js/bootstrap.min.js', destination_root('public/javascripts/bootstrap.min.js')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/fonts/glyphicons-halflings-regular.eot', destination_root('public/fonts/glyphicons-halflings-regular.eot')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/fonts/glyphicons-halflings-regular.svg', destination_root('public/fonts/glyphicons-halflings-regular.svg')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/fonts/glyphicons-halflings-regular.ttf', destination_root('public/fonts/glyphicons-halflings-regular.ttf')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/fonts/glyphicons-halflings-regular.woff', destination_root('public/fonts/glyphicons-halflings-regular.woff')
+
+
