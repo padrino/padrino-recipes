5b4302d9732a92c39b1c4aa8af66999eca8a3ff1
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2014-02-07 02:37:48 -0300

    Fix raw urls on camorra plugin

--- plugins/camorra_plugin.rb
+++ plugins/camorra_plugin.rb
@@ -3,8 +3,17 @@
 # Created by Felipe Cabargas <felipe@cabargas.com>
 # Copyleft please!
 #
-get "https://github.com/felipecabargas/camorra/raw/master/css/#{file}.css", "app/stylesheets/#{file}.css"
-get "https://github.com/felipecabargas/camorra/raw/master/js/#{file}.css", "public/javascripts/#{file}.js"
+# SCSS
+get "https://raw.github.com/felipecabargas/camorra/master/assets/scss/foundation.scss", "app/stylesheets/foundation.scss"
+get "https://raw.github.com/felipecabargas/camorra/master/assets/scss/normalize.scss", "app/stylesheets/normalize.scss"
+get "https://raw.github.com/felipecabargas/camorra/master/assets/scss/foundation/_functions.scss", "app/stylesheets/foundation/_functions.scss"
+get "https://raw.github.com/felipecabargas/camorra/master/assets/scss/foundation/_settings.scss", "app/stylesheets/foundation/_settings.scss"
+get "https://raw.github.com/felipecabargas/camorra/master/assets/scss/foundation/components/#{file}.scss", "app/stylesheets/foundation/components/#{file}.scss"
+
+# JS
+get "https://raw.github.com/felipecabargas/camorra/master/assets/js/#{file}.js", "public/javascripts/#{file}.js"
+get "https://raw.github.com/felipecabargas/camorra/master/assets/js/vendor/#{file}.js", "public/javascripts/vendor/#{file}.js"
+get "https://raw.github.com/felipecabargas/camorra/master/assets/js/foundation/#{file}.js", "public/javascripts/foundation/#{file}.js"
 
 inject_into_file destination_root('app/stylesheets/aplication.scss'), "// Load normalize.scss as recommended by Zurb Foundation\n
 @import 'normalize';\n\n// Customize certain layout settings in foundation/_settings.scss\n@import 'foundation/settings';\n\n// Use Zurb Foundation\n@import 'foundation';\n", :after => "// @include blueprint\n"
