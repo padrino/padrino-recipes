bee03d066169a3595244b193c70c89ffa2ed0046
Darío Javier Cravero <dario@uxtemple.com>
2014-06-20 23:36:41 +0100

    Added bootstrap map and min CSS resources
    Fixes #64

--- plugins/bootstrap_plugin.rb
+++ plugins/bootstrap_plugin.rb
@@ -1,7 +1,11 @@
 # Simple plugin to install Twitter bootstrap.
 #
 get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap.css', destination_root('public/stylesheets/bootstrap.css')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap.css.map', destination_root('public/stylesheets/bootstrap.css.map')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap.css.min', destination_root('public/stylesheets/bootstrap.css.min')
 get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap-theme.css', destination_root('public/stylesheets/bootstrap-theme.css')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap-theme.css.map', destination_root('public/stylesheets/bootstrap-theme.css.map')
+get 'https://raw.github.com/twbs/bootstrap/master/dist/css/bootstrap-theme.css.min', destination_root('public/stylesheets/bootstrap-theme.css.min')
 get 'https://raw.github.com/twbs/bootstrap/master/dist/js/bootstrap.js', destination_root('public/javascripts/bootstrap.js')
 get 'https://raw.github.com/twbs/bootstrap/master/dist/js/bootstrap.min.js', destination_root('public/javascripts/bootstrap.min.js')
 get 'https://raw.github.com/twbs/bootstrap/master/dist/fonts/glyphicons-halflings-regular.eot', destination_root('public/fonts/glyphicons-halflings-regular.eot')
