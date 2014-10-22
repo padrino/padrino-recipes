e0d613734f9faf0420d6f4d1b4f34d9995d31e82
Steven Wagner <steven.wagner@disney.com>
2014-10-22 11:24:35 -0700

    fakeweb support deprecated by vcr in favor of webmock

--- plugins/vcr_plugin.rb
+++ plugins/vcr_plugin.rb
@@ -6,7 +6,7 @@ VCR = <<-CONF
 
 VCR.config do |c|
   c.cassette_library_dir = File.expand_path('../fixtures/', __FILE__)
-  c.stub_with :fakeweb
+  c.stub_with :webmock
   c.default_cassette_options = { :record => :new_episodes }
 end
 CONF
@@ -15,4 +15,4 @@ test = fetch_component_choice(:test)
 path = test == 'rspec' || test == 'cucumber' ? 'spec/spec_helper.rb' : 'test/test_config.rb'
 
 append_file destination_root(path), VCR
-require_dependencies 'vcr', 'fakeweb', :group => :test
+require_dependencies 'vcr', 'webmock', :group => :test
